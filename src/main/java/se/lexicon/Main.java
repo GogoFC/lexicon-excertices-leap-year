3be5f87adaaa0104b6e0a59e2fb51b8575d7140c
Goran Muminovic <goranmuminovic@gmail.com>
2023-11-14 05:02:49 +0100

    branching out

---
+++ src/main/java/se/lexicon/Main.java
@@ -0,0 +1,57 @@
+package se.lexicon;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner yearNumber = new Scanner(System.in);
+        System.out.println("Enter a 4 Digit Year number to check whether it is a Leap Year");
+
+        int year = yearNumber.nextInt();
+
+        boolean isDivisibleBy4 = (year % 4 == 0);
+        boolean isDivisibleBy100 = (year % 100 == 0);
+        boolean isDivisibleBy400 = (year % 400 == 0);
+
+
+        if (isDivisibleBy4) && (isDivisibleBy400){
+            System.out.println("Leap Year");
+        } /*else if (!isDivisibleBy4){
+            System.out.println("Not a Leap Year");
+        }*/ else if (isDivisibleBy100) && (!isDivisibleBy400){
+            System.out.println("Not a Leap Year");
+        } else {
+            System.out.println("Not a Leap Year");
+        }
+
+
+
+
+
+        //long whatInTheWorld = (year % 4);
+
+
+
+        //System.out.println(whatInTheWorld);
+
+        //int modulus = 12 % 3;
+        //System.out.println(modulus);
+    }
+}
+
+
+
+
+
+
+
+/*
+
+What's a leap year
+
+https://www.mathsisfun.com/leap-years.html
+
+https://www.shiksha.com/online-courses/articles/leap-year-in-java/#:~:text=Check%20Leap%20Year%20in%20Java%20using%20Ternary%20Operator&text=Use%20boolean%20condition%20year%20%25%204,is%20not%20a%20leap%20year%E2%80%9D.
+
+ */
